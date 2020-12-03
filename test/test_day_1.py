9752bd48cc3c30659f05639051f96a227ab6f97a
funcelot <artur.mustafin@gmail.com>
2020-12-03 10:49:52 +0300

    fix day 1 test part 2

--- test/test_day_1.py
+++ test/test_day_1.py
@@ -4,7 +4,7 @@ from src.day_1 import fix_report, fix_report2
 
 from .utils import get_data, iterator
 
-def test_fix_day_1():
+def test_day_1_part1():
     """
     In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 
     1721 * 299 = 514579, so the correct answer is 514579.
@@ -14,14 +14,14 @@ def test_fix_day_1():
     assert result
     assert  result[0] * result[1] == 514579
 
-def test_fix_report_day_1():
+def test_day_1_dataset_part1():
     values = get_data("/test/data/day_1")
     result = fix_report(2020, values, iterator(values))
     assert result
     assert result[0] * result[1] == 956091
 
 
-def test_fix_day_2():
+def test_day_1_part2():
     """
     Using the above example again, the three entries that sum to 2020 are 979, 366, and 675. Multiplying them 
     together produces the answer, 241861950.
@@ -30,3 +30,10 @@ def test_fix_day_2():
     result = fix_report2(2020, values, iterator(values))
     assert result
     assert result[0] * result[1] * result[2] == 241861950
+
+
+def test_day_1_dataset_part2():
+    values = get_data("/test/data/day_1")
+    result = fix_report2(2020, values, iterator(values))
+    assert result
+    assert result[0] * result[1] * result[2] == 46767014
